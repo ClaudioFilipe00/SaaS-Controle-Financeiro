09b4a998229af74c208c6d7e7609db207cec1f48
Claudio <fih198@hotmail.com>
2026-03-11 15:55:00 -0300

    atualizando PostGre

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,17 +1,25 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 export const sequelize = new Sequelize(
-    process.env.DB_NAME || "sistema_financeiro",
-    process.env.DB_USER || "teste",
-    process.env.DB_PASSWORD || "",
-    {
-        host: process.env.DB_HOST || "localhost",
-        port: process.env.DB_PORT ? Number(process.env.DB_PORT): 3306,
-        dialect: "mysql",
-        logging:false,
+  process.env.DB_NAME || "postgres",
+  process.env.DB_USER || "postgres",
+  process.env.DB_PASSWORD || "",
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+    dialect: "postgres",
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
+  }
 );
 
 export async function connectDB() {
